78053e5434737b206ef50f483f3e828391c4f27f
Daniel Harten <danielharten@Daniels-MacBook-Air.local>
2024-10-25 10:25:29 +0200

    fix(name): include package name

--- notify-slack.js
+++ notify-slack.js
@@ -23,14 +23,14 @@ const formatMessage = (version, messageType, packageName) => {
   if (messageType === 'success') {
     // Slack likes fewer spaces around new lines for proper formatting
     return (
-      `🎉 *Release Successful!*\n\n` +
+      `🎉 Release Successful! 🎉\n\n` +
       `Package: ${packageName}\n` +
       `Version: ${version}\n\n` +
       `Great job! Wunderbar! Mazel Tov! 🚀`
     );
   } else {
     return (
-      `❌ Release Failed\n\n` + // Single asterisks should bold "Release Failed"
+      `❌ Release Failed ❌\n\n` + // Single asterisks should bold "Release Failed"
       `Package: ${packageJson.name}\n` +
       `Version: ${version}\n\n` + // Same formatting for version
       `Please review the release process and try again. Reach out if help is needed. :warning:`
